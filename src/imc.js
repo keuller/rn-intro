15cf9b6e8c5cdc6bf28e2eb1432066c64e5a169b
Keuller <keuller.magalhaes@gmail.com>
2020-11-07 13:33:13 -0300

    Adicionado tabela do IMC.

---
+++ src/imc.js
@@ -0,0 +1,34 @@
+
+const imc = [
+    {
+        id: '12345',
+        intervalo: {
+            minimo: 14.0,
+            maximo: 15.9
+        },
+        status: 'Muito magro'
+    }, {
+        id: '23456',
+        intervalo: {
+            minimo: 16.0,
+            maximo: 16.9
+        },
+        status: 'Magreza moderada'
+    }, {
+        id: '34567',
+        intervalo: {
+            minimo: 18.5,
+            maximo: 24.9
+        },
+        status: 'Saudavel'
+    }, {
+        id: '456789',
+        intervalo: {
+            minimo: 25.0,
+            maximo: 29.9
+        },
+        status: 'Excesso de peso'
+    }
+]
+
+export default imc;
