917c630f09272463a8899def8802f7d3d0dbd390
Keuller <keuller.magalhaes@gmail.com>
2020-11-07 12:15:05 -0300

    Styles.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import styles from './AppStyles';
 import { ScrollView, View, Text } from 'react-native';
 
 const App = () => (
-      <ScrollView style={styles.scrollView}>
-
-            <Text style={styles.titulo}>Ola Pos-Univem</Text>
-
-      </ScrollView>
+    <ScrollView contentContainerStyle={styles.scrollView}>
+        <Text style={styles.titulo}>Ola Turma P&oacute;s-Univem!</Text>
+    </ScrollView>
 );
 
 export default App;
--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,47 +1,18 @@
-import { StyleSheet, Colors } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#DEDEDE',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: '#DEDEDE',
-  },
-  titulo: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    color: 'darkblue'
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#000',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: '#DDD',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: '#DDD',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    scrollView: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#DEDEDE',
+    },
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'darkblue',
+        paddingTop: 4,
+        paddingBottom: 4
+    }
 });
 
 export default styles;
